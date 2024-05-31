6cee99bac958b50f15952c9b9df7cdea6641baa9
Antonio <antonio20021228@gmail.com>
2024-05-31 10:20:29 +0800

    Revert "trying something"
    This reverts commit a296ca4735ab298ae1edfbceba4f55cbedafc5b1.

--- face_run.py
+++ face_run.py
@@ -1,10 +1,8 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 from facefuncs import get_path_return_output
-import time
-# 記錄開始時間
-start_time = time.time()
-image_path1 = "human_picture\\000002.jpg"
+
+image_path1 = "human_picture\\000100.jpg"
 highest_score_image, highest_score_image_path = get_path_return_output(
     image_path1)
 img = Image.open(highest_score_image_path)
